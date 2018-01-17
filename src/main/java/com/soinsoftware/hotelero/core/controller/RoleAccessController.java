3d4758e6867adf082698a83e8c359a6f09589a71
crodriguez <crodriguez@carlosrodriguez-inew>
2018-01-17 16:50:10 -0500

    Role X Access controller class

---
+++ src/main/java/com/soinsoftware/hotelero/core/controller/RoleAccessController.java
@@ -0,0 +1,39 @@
+package com.soinsoftware.hotelero.core.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.soinsoftware.hotelero.persistence.bll.RoleAccessBll;
+import com.soinsoftware.hotelero.persistence.entity.RoleAccess;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * @author Carlos Rodriguez
+ * @since 1.0.0
+ */
+@Log4j
+public class RoleAccessController {
+
+	public List<RoleAccess> select() {
+		try {
+			final RoleAccessBll bll = new RoleAccessBll();
+			final List<RoleAccess> roleAccesses = bll.selectAll(true);
+			bll.closeDbConnection();
+			return roleAccesses;
+		} catch (final IOException ex) {
+			log.error(ex.getMessage());
+			return null;
+		}
+	}
+
+	public void save(final RoleAccess roleAccess) {
+		try {
+			final RoleAccessBll bll = new RoleAccessBll();
+			bll.save(roleAccess);
+			bll.closeDbConnection();
+		} catch (final IOException ex) {
+			log.error(ex.getMessage());
+		}
+	}
+}
